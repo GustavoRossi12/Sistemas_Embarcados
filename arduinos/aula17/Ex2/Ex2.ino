9b3e288d5dadab5cd20274ecd7193462da0b8637
Gustavo_Rossi <gustavo.259523@alunosatc.edu.br>
2025-06-23 17:05:40 -0300

    Update Ex2.ino

--- arduinos/aula17/Ex2/Ex2.ino
+++ arduinos/aula17/Ex2/Ex2.ino
@@ -6,12 +6,11 @@
 #define pin_b2 3
 #define pin_b3 4
 
-//Aqui coloco o tipo da variavel (INT) e coloco o valor inicial dela como 0 (fora do "VOID SETUP" para que ele não resete toda vez que o codigo voltar para cima)
-int c = 0;
+
 int t1 = 0;
 int t2 = 0;
 
-//começo a programação informando o tipo de pino INPUT(envia dados) ou OUTPUT(recebe dados)
+
 void setup() {
   pinMode(led_LIB, OUTPUT);
   pinMode(led_LOT, OUTPUT);
@@ -19,67 +18,60 @@ void setup() {
   pinMode(pin_b1, INPUT);
   pinMode(pin_b2, INPUT);
   pinMode(pin_b3, INPUT);
-  //codigo abaixo é a frequencia que o arduino trabalhara
+  
   Serial.begin(9600);
 }
 
 void loop() {
-  //aqui informo o nome da variavel e o tipo dela que sera boleana, sendo 1 ou 0 como resultado. (coloquei esses nomes por conta do enunciado da questão)
-  bool SEV = digitalRead(pin_b1); //pin_botaoA esta com o nome de "SEV"
-  bool SSV = digitalRead(pin_b2); //pin_botaoB esta com o nome de "SSV"
-  bool BOT = digitalRead(pin_b3); //pin_botaoC esta com o nome de "BOT"
 
-  //aqui sera a mensagem que sera mostrada no MONITOR SERIAL
+  bool SEV = digitalRead(pin_b1);
+  bool SSV = digitalRead(pin_b2); 
+  bool BOT = digitalRead(pin_b3); 
+
   Serial.println(c);
 
-  //Aqui quando o botão SEV for pressionado e o tic1 for igual a 0 ele ira ver se o contador chegou a 10, caso não ocorra ele ira continuar, mas se chegar ele para o contador
  if (SEV == 1 && t1 == 0 && c<10) {
-    //apos a "verificação" ele ira acressentar "1" no CONT e no TIC1, para dizer que o botão só foi pressionado uma vez
+   
     c += 1;
     t1 += 1;
   }
-  //Quando o botão SEV for igual a 0 o TIC1 tambem sera ja que o botão não esta pressionado
+ 
   if (SEV == 0) {
     t1 = 0;
   }
 
-  //Aqui quando o botão SEV for pressionado e o tic2 for igual a 0 ele ira ver se o contador chegou a 0, caso não ocorra ele ira continuar, mas se chegar ele para o contador  
   if (SSV == 1 && t2 == 0 && c>0) {
-    //apos a "verificação" ele ira diminuir "1" no CONT e acressentar no TIC1, para dizer que o botão só foi pressionado uma vez
+  
     c -= 1;
     t2 += 1;
   }
 
-  //Quando o botão SEV for igual a 0 o TIC2 tambem sera ja que o botão não esta pressionado
+  
   if (SSV == 0) {
     t2 = 0;
   }
 
-//caso o contador chegar a 10 o "led_LOT" sera acesso para informar que o estacionamento esta cheio
 if (c == 10){
   digitalWrite(led_LOT, 1);
   digitalWrite(led_LIB, 0);
-}else{ //caso isso nao ocorra ele continuara normal
+}else{ 
   digitalWrite(led_LOT, 0);
 }
 
-//caso o contador for menor a 10 o "led_LIB" sera acesso para informar que à vaga no estacionamento
 if (c < 10){
   digitalWrite(led_LIB, 1);
   digitalWrite(led_LOT, 0);
-}else{ //caso isso nao ocorra ele continuara normal
+}else{ 
   digitalWrite(led_LIB, 0);
 }
 
-//caso o contador chegar a 0 o "led_VER" sera acesso para informar para o operador verificar se esta tudo certo
 if (c == 0){
   digitalWrite(led_VER, 1);
   digitalWrite(led_LOT, 0);
-}else{ //caso isso nao ocorra ele continuara normal
+}else{
   digitalWrite(led_VER, 0);
 }
 
-//se o botão BOT for precisonado ele resetara as informações, voltando a contagem para 0
 if (BOT == 1) {
   c = 0;
 }
