147d578b7d71a4cc41f3cd86192606105a823edf
Gustavo_Rossi <gustavo.259523@alunosatc.edu.br>
2025-06-23 20:18:46 -0300

    Update atividade.ino

--- arduinos/aula13/atividade.ino
+++ arduinos/aula13/atividade.ino
@@ -5,11 +5,11 @@
 
 
 void setup() {
-  Serial.begin(9600);          //define a velocidade da comunicação
-  pinMode (pin_led1, OUTPUT);  //definindo led1 saida
-  pinMode (pin_led2, OUTPUT);  //definindo led2 saida
-  pinMode (pin_led3, OUTPUT);  //definindo led3 saida
-  pinMode (pin_led4, OUTPUT);  //definindo led4 saida
+  Serial.begin(9600);       
+  pinMode (pin_led1, OUTPUT); 
+  pinMode (pin_led2, OUTPUT); 
+  pinMode (pin_led3, OUTPUT); 
+  pinMode (pin_led4, OUTPUT);  
 
 }
 
@@ -18,18 +18,18 @@ void loop() {
     char i = Serial.read();
     serial.println(i);
   }
- /*
- Serial.println("liga LED");    //manda mensagem pro serial monitor sempre que acontece isso
- digitalWrite (pin_led1, HIGH);  //definindo led1 alto
- digitalWrite (pin_led2, HIGH);  //definindo led2 alto
- digitalWrite (pin_led3, HIGH);  //definindo led3 alto
- digitalWrite (pin_led4, HIGH);  //definindo led4 alto
- delay (500);                    // espera 500 milissegundos
- Serial.println("desliga LED");  //manda mensagem pro serial monitor sempre que acontece isso
- digitalWrite (pin_led1, LOW);   //definindo led1 baixo
- digitalWrite (pin_led2, LOW);   //definindo led2 baixo
- digitalWrite (pin_led3, LOW);   //definindo led3 baixo
- digitalWrite (pin_led4, LOW);   //definindo led4 baixo
- delay (500);                    // espera 500 milissegundos
-*/
+
+ Serial.println("liga LED");    
+ digitalWrite (pin_led1, HIGH);  
+ digitalWrite (pin_led2, HIGH); 
+ digitalWrite (pin_led3, HIGH);  
+ digitalWrite (pin_led4, HIGH); 
+ delay (500);                    
+ Serial.println("desliga LED");  
+ digitalWrite (pin_led1, LOW);   
+ digitalWrite (pin_led2, LOW);   
+ digitalWrite (pin_led3, LOW);   
+ digitalWrite (pin_led4, LOW);  
+ delay (500);                   
+
 }
