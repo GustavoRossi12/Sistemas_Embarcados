7c3c8a17ceae04a2313d0d676a2ef4392fc9ead9
Gustavo_Rossi <gustavo.259523@alunosatc.edu.br>
2025-06-23 15:59:53 -0300

    Update and rename impar-4.ino to ex4.ino

---
+++ arduinos/aula16/ex4/ex4.ino
@@ -0,0 +1,51 @@
+#define pin_l1 8
+#define pin_l2 9
+#define pin_l3 10
+#define pin_l4 11
+#define pin_bA 2
+#define pin_bB 3
+#define pin_bC 4
+
+void setup() {
+  pinMode(pin_l1, OUTPUT);
+  pinMode(pin_l2, OUTPUT);
+  pinMode(pin_l3, OUTPUT);
+  pinMode(pin_l4, OUTPUT);
+  pinMode(pin_bA, INPUT);
+  pinMode(pin_bB, INPUT);
+  pinMode(pin_bC, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  bool A = digitalRead(pin_bA);
+  bool B = digitalRead(pin_bB);
+  bool C = digitalRead(pin_bC);
+
+  if ((A==0)&&(B==0)&&(C==1)) {
+    digitalWrite(pin_l1, 1);
+  } else {
+    digitalWrite(pin_l1, 0);
+  }
+
+
+  if ((A==0)&&(B==1)&&(C==0)) {
+    digitalWrite(pin_l1, 1);
+  } else {
+    digitalWrite(pin_l1, 0);
+  }
+
+
+  if ((A==1)&&(B==0)&&(C==0)) {
+    digitalWrite(pin_l1, 1);
+  } else {
+    digitalWrite(pin_l1, 0);
+  }
+
+
+  if ((A==1)&&(B==1)&&(C==1)) {
+    digitalWrite(pin_l1, 1);
+  } else {
+    digitalWrite(pin_l1, 0);
+  }
+}
